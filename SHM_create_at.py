1b1901ddc94e19ccf1aec2cc6cc18ed1cae3cd86
Justin Welsh Tam <jt.tam514@gmail.com>
2020-11-25 18:28:54 -0500

    Fixed total time error

--- SHM_create_at.py
+++ SHM_create_at.py
@@ -8,8 +8,8 @@ def shm_accel(A, w, total_time, val_per_second):
     ti = total_time*val_per_second
     at = np.zeros((2, ti))
     for i in range(ti):
-        t = total_time*i/ti
-        at[0, i] = total_time*i/ti
+        t = total_time*i/(ti-1)
+        at[0, i] = t
         at[1, i] = -A*w**2*math.cos(w*t)
     np.savetxt('SHM_acceleration.csv', at.transpose(), delimiter=',')
     return at
@@ -18,8 +18,8 @@ def shm_accel(A, w, total_time, val_per_second):
 if __name__ == '__main__':
     A = 1
     w = 4*math.pi
-    total_time = 2 # seconds
-    val_per_second = 10000
+    total_time = 3 # seconds
+    val_per_second = 101 # Hz
     at = shm_accel(A, w, total_time, val_per_second)
     plt.plot(at[0], at[1])
     plt.show()

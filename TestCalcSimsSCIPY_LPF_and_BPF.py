a684421ae5f9dc11d63333cc37b72dd0b82dfffa
Justin Welsh Tam <jt.tam514@gmail.com>
2020-12-03 01:42:06 -0500

    Added high pass filters to v(t) and x(t)

--- TestCalcSimsSCIPY_LPF_and_BPF.py
+++ TestCalcSimsSCIPY_LPF_and_BPF.py
@@ -2,27 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal, integrate
 
-file_name_g = '2020-11-26 21_41_35.814828_LOG1.csv'
+file_name_g = 'TestLastData.csv'
 
 
-def butter_highpass_filter(data, normal_cutoff, fs, order):
+def butter_filter(data, normal_cutoff,  order, type):
     # Get the filter coefficients
-    b, a = signal.butter(order, normal_cutoff, btype='low')
+    b, a = signal.butter(order, normal_cutoff, btype=type)
     y = signal.filtfilt(b, a, data)
     return y
 
 
-def acc_preprocess(at, t, ti):
+def acc_post_process(at, t, ti):
     fc = 5  # cut off frequency Hz
     fs = int(ti/t[-1, 0])
     normalized_fc = 2*fc/fs
     at_mavg = np.zeros((ti, 1))
     #plot_1(at[:, 0], t, 'Acceleration before moving average :)')
-    window = 50  # must be even
+    window = 2  # must be even
     for i in range(int(window/2), ti-int(window/2)):
         at_mavg[i, 0] = np.sum(at[i-int(window/2):i + int(window/2), 0])/window
     #plot_1(at_mavg[:, 0], t, 'Acceleration after moving average :)')
-    at_mavg[:, 0] = butter_highpass_filter(at_mavg[:, 0], normalized_fc, fs, 2)
+    at_mavg[:, 0] = butter_filter(at_mavg[:, 0], normalized_fc, 1, 'low')
     return at_mavg
 
 
@@ -31,7 +31,6 @@ def read_csv(file_name, dim):
     ti = np.shape(at_t12)[0]
     if ti % 2 == 1:
         ti = ti - 1
-    at_t = np.zeros((ti, dim))
     at_t = at_t12[:ti, :]
     t = np.zeros((ti, 1))
     at = np.zeros((ti, dim))
@@ -40,7 +39,7 @@ def read_csv(file_name, dim):
     dti = np.zeros((ti-1, 1))  # debugging tool
     for i in range(ti-1):
         dti[i] = t[i+1]-t[i]
-    at = acc_preprocess(at, t, ti)
+    at = acc_post_process(at, t, ti)
     return at, t, ti
 
 
@@ -49,11 +48,17 @@ def double_simps_integral(x0, v0, dim, file_name):
     at = np.zeros((ti, dim)); at[:, 0] = at1[:, 0]
     xt = np.zeros((ti, dim))
     vt = np.zeros((ti, dim))
-    dt = 0.006  # seconds, hard set for MSU tracker
+    freq_factor = 2/int(ti/t[-1, 0])
     for d in range(dim):
         for i in range(1, ti):
             vt[i, d] = integrate.simps(at[:i+1, d], x=t[:i+1, 0]) + v0[0]
+    vt[:, 0] = butter_filter(vt[:, 0], 0.2*freq_factor, 1, 'high')
+    #vt[:, 0] = butter_filter(vt[:, 0], 6*freq_factor, 1, 'low')
+    for d in range(dim):
+        for i in range(1, ti):
             xt[i, d] = integrate.simps(vt[:i+1, d], x=t[:i+1, 0]) + x0[0]
+    #xt[:, 0] = butter_filter(xt[:, 0], 0.7*freq_factor, 1, 'high')
+    #xt[:, 0] = butter_filter(xt[:, 0], 4*freq_factor, 1, 'low')
     return xt, vt, at, t
 
 
